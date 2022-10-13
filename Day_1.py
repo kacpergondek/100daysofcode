03a3a70e5176c961e3fb7b8f39fac61d4771338c
Kacper Gondek <69770687+kacpergondek@users.noreply.github.com>
2022-10-13 22:57:00 +0100

    Day 1 Band Name Generator
    Manipulating strings and variables to generate a band name

---
+++ Day_1.py
@@ -0,0 +1,4 @@
+print ("Band name generator")
+city = input ("Please enter the city you grew up in \n")
+pet_name = input ("Please enter your pet name \n")
+print ("Your band name is " + city + " " + pet_name )
